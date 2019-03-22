a5cb406b2134f783b400f1eac22e77b9b24e47e2
John Balke <john.balke@gmail.com>
2019-03-22 11:13:37 +0000

    Added a 404 page

--- main.go
+++ main.go
@@ -12,6 +12,9 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "<h1>Welcome to my site!</h1>")
 	} else if r.URL.Path == "/contact" {
 		fmt.Fprint(w, `<h2>To get in touch, please send an email to <a href="mailto:support@lenslocked.com">support@lenslocked.com</a></h2>`)
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, `<h1>We could not find the page you were looking for :(</h1><p>Please email us if you keep being sent to an invalid page.`)
 	}
 }
 
