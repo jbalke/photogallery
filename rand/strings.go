96f7728f7ee70e7719ad77d06d012a0ec8ed9bcf
John Balke <john.balke@gmail.com>
2019-04-04 17:55:30 +0100

    Created rand package that enables creation of random strings and remember tokens

---
+++ rand/strings.go
@@ -0,0 +1,36 @@
+package rand
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+)
+
+// RememberTokenBytes is the length of the byte slice used for generating the remember token.
+const RememberTokenBytes = 32
+
+// Bytes will generate n random bytes or return an error.
+// This uses the crypto/rand package so is safe for
+// generating tokens.
+func Bytes(n int) ([]byte, error) {
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+	if err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
+// String returns a URLencoded string representation of a byte slice of nBytes length.
+func String(nBytes int) (string, error) {
+	b, err := Bytes(nBytes)
+	if err != nil {
+		return "", err
+	}
+	return base64.URLEncoding.EncodeToString(b), nil
+}
+
+// RememberToken is a helper function to generate remember tokens
+// of a predetermined size.
+func RememberToken() (string, error) {
+	return String(RememberTokenBytes)
+}
