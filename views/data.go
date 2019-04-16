12e9b0b219f805e791f77fe4f715c5b6951cce92
John Balke <jbalke@users.noreply.github.com>
2019-04-16 12:47:12 +0100

    Updated SetAlert to log non-public errors

--- views/data.go
+++ views/data.go
@@ -1,5 +1,7 @@
 package views
 
+import "log"
+
 const (
 	AlertLvlError   = "danger"
 	AlertLvlWarning = "warning"
@@ -31,6 +33,7 @@ func (d *Data) SetAlert(err error) {
 			Message: pErr.Public(),
 		}
 	} else {
+		log.Println(err)
 		d.Alert = &Alert{
 			Level:   AlertLvlError,
 			Message: AlertMsgGeneric,
